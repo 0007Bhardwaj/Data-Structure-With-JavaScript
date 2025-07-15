aaa1bc5c3049e26d4ff407c55a2c4e534e612056
0007Bhardwaj <97664483+0007Bhardwaj@users.noreply.github.com>
2025-07-15 14:05:22 +0530

    Update Two_Pointers.js

--- Array/Important_Algorithums/Two_Pointers.js
+++ Array/Important_Algorithums/Two_Pointers.js
@@ -304,3 +304,30 @@ function maxProfit(prices, K) {
 
   return maxProfit > 0 ? prices.slice(bestBuy, bestSell + 1) : [];
 }
+
+
+
+// 1 - LONGEST SUBSTRING WITHOUT REPEATING CHARACTERS
+
+function longestSubstringWithoutRepeatingCharacters(string = "") {
+  let start = 0;
+  let end = 0;
+  let maxLength = 0;
+  const uniqueSubString = new Set();
+
+  while (end < string.length) {
+    if (!uniqueSubString.has(string[end])) {
+      uniqueSubString.add(string[end]);
+      end++;
+      maxLength = Math.max(maxLength, uniqueSubString.size);
+    } else {
+      uniqueSubString.delete(string[start]);
+      start++;
+    }
+  }
+
+  return maxLength;
+}
+
+
+
